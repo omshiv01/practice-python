fd5b6f7d364bcb761b651cf9a024aab2656bf771
omshiv01 <wasalugovind@gmail.com>
2024-01-31 21:33:08 +0530

    my frist commit

---
+++ intro2.py
@@ -0,0 +1,2 @@
+print('my self om naidu \nim studying btech in sbjain collage\n3rd year\nmechenical engg\nfurther i generated my intrest in programing language and chose my full time carer as devoloper')
+print('\n:-i have skill like : \n 1.c \n 2.c++ \n 3.r programing \n 4.matlab \n 5.html\n 6.web devolopment using django')
